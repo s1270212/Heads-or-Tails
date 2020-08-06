815098599c969d003379dfc062f21eebf69a5306
s1270212 <s1270212@u-aizu.ac.jp>
2020-08-06 21:49:44 +0900

    Victory message added

---
+++ print.java
@@ -0,0 +1,9 @@
+public class print{
+    public static void main(String[] args){
+        int cnt0;
+        int cnt1;
+        if(cnt0>cnt1){
+            System.out.println("You won");
+        }else System.out.println("You lost");
+    }
+}
